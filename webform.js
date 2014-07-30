77b11a2e787cc804b3bf11d3396493a0de458e05
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-07-30 09:08:54 -0400

    Move component widget builders to their own individual functions.

--- webform.js
+++ webform.js
@@ -59,9 +59,9 @@ function webform_entity_post_render_content(entity, entity_type, bundle) {
  */
 function webform_form(form, form_state, entity, entity_type, bundle) {
   try {
-    // @TODO Make a component widget form function for each component type to
-    //       make this function more maintainable.
-    // @TODO Add support for component weight (ordering).
+    // @TODO Add support for component weight (ordering), why doesn't the DGFAPI
+    //       handle this? It should, but maybe it only supports fields and extra
+    //       fields.
     /**
      * SUPPORTED COMPONENTS
      * [x] Date
@@ -79,241 +79,58 @@ function webform_form(form, form_state, entity, entity_type, bundle) {
      * [x] Time
      */
     dpm(entity.webform);
-    // Attach the entity to the form.
+    
+    // Attach the webform to the form.
     form.webform = entity.webform;
+    
     // Place the webform components on the entity content.
     $.each(entity.webform.components, function(cid, component) {
+        
+        dpm(component.name);
         dpm(component);
-        var type = component.type;
-        var options = { };
-        var attributes = { };
-        var children = [];
-        var required = parseInt(component.mandatory) == 1 ? true : false;
-        var element_id = drupalgap_form_get_element_id(component.form_key, form.id);
-        var markup = null;
-        // Extract the value.
-        var value = component.value;
-        // Replace the tokens.
-        value = webform_tokens_replace(value);
-        // Does the user have access to this component.
-        var access = true;
-        if (
-          component.extra['private'] &&
-          !user_access('access all webform results')
-        ) { access = false; }
-        // Display the label?
+        
+        // Preset some component element variables.
         var title = component.name;
         if (component.extra.title_display == 'none') { title = ''; }
-        // Some component types map cleanly, others do not, make adjustments here.
-        switch (type) {
-          case 'date':
-            // We'll turn this element into a hidden field, and use children to
-            // make the widget(s) to power this component.
-            type = 'hidden';
-            // Month field.
-            var month = {
-              type: 'select',
-              required: required,
-              options: {
-                1: 'Jan',
-                2: 'Feb',
-                3: 'Mar',
-                4: 'Apr',
-                5: 'May',
-                6: 'Jun',
-                7: 'Jul',
-                8: 'Aug',
-                9: 'Sep',
-                10: 'Oct',
-                11: 'Nov',
-                12: 'Dec',
-                attributes: {
-                  id: element_id + '-month',
-                  onchange: "_webform_date_component_onchange('month', '" + element_id + "');"
-                }
-              }
-            };
-            // Day field.
-            var day = {
-              type: 'select',
-              title: title, // Place the title on the first child since the parent is hidden.
-              required: required,
-              options: {
-                attributes: {
-                  id: element_id + '-day',
-                  onchange: "_webform_date_component_onchange('day', '" + element_id + "');"
-                }
-              }
-            };
-            for (var i = 1; i <= 31; i++) { day.options[i] = i; }
-            // Year field.
-            var year = {
-              options: {
-                attributes: {
-                  id: element_id + '-year',
-                  onchange: "_webform_date_component_onchange('year', '" + element_id + "');"
-                }
-              }
-            };
-            if (component.extra.year_textfield) { year.type = 'number'; }
-            else {
-              year.type = 'select';
-              // Figure out the range of years for the select options.
-              var range = webform_date_component_parse_range(component);
-              if (range) {
-                if (range.start_date.interval != range.end_date.interval) {
-                  console.log('WARNING: webform_form() - only matching start/end intervals supported (' + range.start_date.interval + ' != ' + range.end_date.interval + ')');
-                }
-                var low = null;
-                var high = null;
-                var current_year = parseInt(date('Y'));
-                switch (range.start_date.interval) {
-                  case 'year':
-                  case 'years':
-                    low = current_year + range.start_date.value; // Value is negative here, so we add it to get the low.
-                    high = current_year + range.end_date.value;
-                    break;
-                  default:
-                    console.log('WARNING: webform_form() - unsupported date interval (' + range.start_date.interval + ')');
-                    break;
-                }
-              }
-              for (var i = low; i <= high; i++) { year.options[i] = i; }
-            }
-            // Push the children onto the element.
-            children.push(day);
-            children.push(month);
-            children.push(year);
-            break;
-          case 'markup':
-            markup = component.value;
-            break;
-          case 'number':
-            // If it is not set to an integer, turn it into a textfield. If it
-            // is an int, then set the step, min and max if they are provided.
-            if (!component.extra.integer) { type = 'textfield'; }
-            else {
-              if (!empty(component.extra.min)) { attributes.min = component.extra.min; }
-              if (!empty(component.extra.max)) { attributes.max = component.extra.max; }
-              if (!empty(component.extra.step)) { attributes.step = component.extra.step; }
-            }
-            break;
-          case 'select':
-            // Extract the items (allowed values).
-            var items = component.extra.items.split('\n');
-            // @TODO - The shuffle function works, but the DG Forms API places
-            // the options in order of e.g. an int value.
-            if (component.extra.optrand) { items = shuffle(items); }
-            for (var i = 0; i < items.length; i++) {
-              var parts = items[i].split('|');
-              options[parts[0]] = parts[1];
-            }
-            // What widget are we using?
-            var widget = null;
-            // A select list.
-            if (component.extra.aslist) {
-              if (component.extra.multiple) {
-                // @TODO - when this component is required, the fake 'required'
-                // option comes up in the jQM multiple select widget. We need to
-                // prevent that from happening, in DrupalGap core.
-                // @UPDATE - I think this has been taken care of now that we are
-                // using an empty string for the fake value.
-                attributes['data-native-menu'] = 'false';
-                attributes['multiple'] = 'multiple';
-              }
-            }
-            // Not a select list.
-            else {
-              if (component.extra.multiple) {
-                type = 'checkboxes';
-              }
-              else {
-                type = 'radios';
-              }
-            }
-            break;
-          case 'time':
-            // We'll turn this element into a hidden field, and use children to
-            // make the widget(s) to power this component.
-            type = 'hidden';
-            // Hours
-            var hours = {
-              type: 'select',
-              title: title, // Place the title on the first child since the parent is hidden.
-              options: {
-                attributes: {
-                  id: element_id + '-hours',
-                  onchange: "_webform_time_component_onchange('hours', '" + element_id + "');"
-                }
-              }
-            };
-            var time_format = null;
-            if (component.extra.hourformat == '12-hour') {
-              for (var i = 1; i <= 12; i++) { hours.options[i] = i; }
-              time_format = {
-                type: 'radios',
-                value: 'am',
-                options: {
-                  am: 'am',
-                  pm: 'pm',
-                  attributes: {
-                    id: element_id + '-ampm',
-                    onchange: "_webform_time_component_onchange('ampm', '" + element_id + "');"
-                  }
-                }
-              }
-            }
-            else if (component.extra.hourformat == '24-hour') {
-              for (var i = 0; i <= 23; i++) { hours.options[i] = i; }
-            }
-            // Minutes
-            var minutes = {
-              type: 'select',
-              options: {
-                attributes: {
-                  id: element_id + '-minutes',
-                  onchange: "_webform_time_component_onchange('minutes', '" + element_id + "');"
-                }
-              }
-            }
-            for (var i = 0; i < 60; i += parseInt(component.extra.minuteincrements)) {
-              var value = i;
-              var label = '' + i;
-              if (label.length == 1) { label = "0" + label; }
-              minutes.options[value] = label;
-            }
-            // Add the children.
-            children.push(hours);
-            children.push(minutes);
-            if (time_format) { children.push(time_format); }
-            break;
-        }
-        // Set any attributes onto the options.
-        options.attributes = attributes;
-        // Build the form element.
+        var required = parseInt(component.mandatory) == 1 ? true : false;
+        var markup = null;
+        var access = (component.extra['private'] && !user_access('access all webform results')) ? false : true;
+        
+        // Build the form element basics.
         form.elements[component.form_key] = {
-          type: type,
+          type: component.type,
           title: title,
           required: required,
-          value: value,
+          value: webform_tokens_replace(component.value),
           description: webform_tokens_replace(component.extra.description),
           disabled: component.extra.disabled,
           access: access,
-          options: options,
-          children: children
+          options: {
+            attributes: {
+              id: drupalgap_form_get_element_id(component.form_key, form.id) // without this, does the DGFAPI add an id?
+            }
+          },
+          children: [],
+          weight: parseInt(component.weight) // Not working...
         };
-        // Handle markup components.
-        if (markup) {
-          form.elements[component.form_key].type = null;
-          form.elements[component.form_key].title = '';
-          form.elements[component.form_key].markup = markup;
+        
+        // Some component types map cleanly, others do not. For those that don't
+        // send them off to the component widget form handler.
+        var function_name = 'webform_component_' + component.type + '_widget_form';
+        if (function_exists(function_name)) {
+          var fn = window[function_name];
+          fn(form, form_state, entity, entity_type, bundle, component, form.elements[component.form_key]);
         }
+
     });
+
+    // Submit button.    
     var submit_text = empty(entity.webform.submit_text) ? 'Submit' : entity.webform.submit_text;
     form.elements['submit'] = {
       type: 'submit',
       value: submit_text
     };
+    
     return form;
   }
   catch (error) { console.log('webform_form - ' + error); }
@@ -379,6 +196,226 @@ function _webform_time_component_onchange(which, input) {
   }
   catch (error) { console.log('_webform_time_component_onchange - ' + error); }
 }
+
+/**
+ * Component Widgets
+ */
+
+/**
+ *
+ */
+function webform_component_date_widget_form(form, form_state, entity, entity_type, bundle, component, element) {
+  try {
+    // We'll turn this element into a hidden field, and use children to
+    // make the widget(s) to power this component.
+    var element_id = element.options.attributes.id;
+    element.type = 'hidden';
+    // Month field.
+    var month = {
+      title: element.title, // Place the title on the first child since the parent is hidden.
+      type: 'select',
+      options: {
+        1: 'Jan',
+        2: 'Feb',
+        3: 'Mar',
+        4: 'Apr',
+        5: 'May',
+        6: 'Jun',
+        7: 'Jul',
+        8: 'Aug',
+        9: 'Sep',
+        10: 'Oct',
+        11: 'Nov',
+        12: 'Dec',
+        attributes: {
+          id: element_id + '-month',
+          onchange: "_webform_date_component_onchange('month', '" + element_id + "');"
+        }
+      }
+    };
+    // Day field.
+    var day = {
+      type: 'select',
+      options: {
+        attributes: {
+          id: element_id + '-day',
+          onchange: "_webform_date_component_onchange('day', '" + element_id + "');"
+        }
+      }
+    };
+    for (var i = 1; i <= 31; i++) { day.options[i] = i; }
+    // Year field.
+    var year = {
+      options: {
+        attributes: {
+          id: element_id + '-year',
+          onchange: "_webform_date_component_onchange('year', '" + element_id + "');"
+        }
+      }
+    };
+    if (component.extra.year_textfield) { year.type = 'number'; }
+    else {
+      year.type = 'select';
+      // Figure out the range of years for the select options.
+      var range = webform_date_component_parse_range(component);
+      if (range) {
+        if (range.start_date.interval != range.end_date.interval) {
+          console.log('WARNING: webform_form() - only matching start/end intervals supported (' + range.start_date.interval + ' != ' + range.end_date.interval + ')');
+        }
+        var low = null;
+        var high = null;
+        var current_year = parseInt(date('Y'));
+        switch (range.start_date.interval) {
+          case 'year':
+          case 'years':
+            low = current_year + range.start_date.value; // Value is negative here, so we add it to get the low.
+            high = current_year + range.end_date.value;
+            break;
+          default:
+            console.log('WARNING: webform_form() - unsupported date interval (' + range.start_date.interval + ')');
+            break;
+        }
+      }
+      for (var i = low; i <= high; i++) { year.options[i] = i; }
+    }
+    // Push the children onto the element.
+    element.children.push(month);
+    element.children.push(day);
+    element.children.push(year);
+  }
+  catch (error) { console.log('webform_component_date_widget_form - ' + error); }
+}
+
+/**
+ *
+ */
+function webform_component_markup_widget_form(form, form_state, entity, entity_type, bundle, component, element) {
+  try {
+    var element_id = element.options.attributes.id;
+    element.type = null;
+    element.title = '';
+    element.markup = component.value;
+  }
+  catch (error) { console.log('webform_component_markup_widget_form - ' + error); }
+}
+
+/**
+ *
+ */
+function webform_component_number_widget_form(form, form_state, entity, entity_type, bundle, component, element) {
+  try {
+    var element_id = element.options.attributes.id;
+    // If it is not set to an integer, turn it into a textfield. If it
+    // is an int, then set the step, min and max if they are provided.
+    if (!component.extra.integer) { element.type = 'textfield'; }
+    else {
+      if (!empty(component.extra.min)) { element.options.attributes.min = component.extra.min; }
+      if (!empty(component.extra.max)) { element.options.attributes.max = component.extra.max; }
+      if (!empty(component.extra.step)) { element.options.attributes.step = component.extra.step; }
+    }
+  }
+  catch (error) { console.log('webform_component_number_widget_form - ' + error); }
+}
+
+/**
+ *
+ */
+function webform_component_select_widget_form(form, form_state, entity, entity_type, bundle, component, element) {
+  try {
+    var element_id = element.options.attributes.id;
+    // Extract the items (allowed values).
+    var items = component.extra.items.split('\n');
+    // @TODO - The shuffle function works, but the DG Forms API places
+    // the options in order of e.g. an int value.
+    if (component.extra.optrand) { items = shuffle(items); }
+    for (var i = 0; i < items.length; i++) {
+      var parts = items[i].split('|');
+      element.options[parts[0]] = parts[1];
+    }
+    // A select list.
+    if (component.extra.aslist) {
+      if (component.extra.multiple) {
+        // @TODO - when this component is required, the fake 'required'
+        // option comes up in the jQM multiple select widget. We need to
+        // prevent that from happening, in DrupalGap core.
+        // @UPDATE - I think this has been taken care of now that we are
+        // using an empty string for the fake value.
+        element.options.attributes['data-native-menu'] = 'false';
+        element.options.attributes['multiple'] = 'multiple';
+      }
+    }
+    // Not a select list.
+    else {
+      if (component.extra.multiple) { element.type = 'checkboxes'; }
+      else { element.type = 'radios'; }
+    }
+  }
+  catch (error) { console.log('webform_component_select_widget_form - ' + error); }
+}
+
+/**
+ *
+ */
+function webform_component_time_widget_form(form, form_state, entity, entity_type, bundle, component, element) {
+  try {
+    // We'll turn this element into a hidden field, and use children to
+    // make the widget(s) to power this component.
+    element.type = 'hidden';
+    var element_id = element.options.attributes.id;
+    // Hours
+    var hours = {
+      type: 'select',
+      title: element.title, // Place the title on the first child since the parent is hidden.
+      options: {
+        attributes: {
+          id: element_id + '-hours',
+          onchange: "_webform_time_component_onchange('hours', '" + element_id + "');"
+        }
+      }
+    };
+    var time_format = null;
+    if (component.extra.hourformat == '12-hour') {
+      for (var i = 1; i <= 12; i++) { hours.options[i] = i; }
+      time_format = {
+        type: 'radios',
+        value: 'am',
+        options: {
+          am: 'am',
+          pm: 'pm',
+          attributes: {
+            id: element_id + '-ampm',
+            onchange: "_webform_time_component_onchange('ampm', '" + element_id + "');"
+          }
+        }
+      }
+    }
+    else if (component.extra.hourformat == '24-hour') {
+      for (var i = 0; i <= 23; i++) { hours.options[i] = i; }
+    }
+    // Minutes
+    var minutes = {
+      type: 'select',
+      options: {
+        attributes: {
+          id: element_id + '-minutes',
+          onchange: "_webform_time_component_onchange('minutes', '" + element_id + "');"
+        }
+      }
+    }
+    for (var i = 0; i < 60; i += parseInt(component.extra.minuteincrements)) {
+      var value = i;
+      var label = '' + i;
+      if (label.length == 1) { label = "0" + label; }
+      minutes.options[value] = label;
+    }
+    // Add the children.
+    element.children.push(hours);
+    element.children.push(minutes);
+    if (time_format) { element.children.push(time_format); }
+  }
+  catch (error) { console.log('webform_component_time_widget_form - ' + error); }
+}
+
 
 /*********
  * Pages *
