d6ae29fb03295ae03bd5e0f03aeeebb2b88f41a6
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-08-10 12:40:49 -0500

    Move services and helpers to their own files.

---
+++ src/webform.helpers.js
@@ -0,0 +1,92 @@
+/**
+ * HELPERS
+ */
+
+/**
+ * Given a date component, this will return a JSON object containing the date
+ * range.
+ */
+function webform_date_component_parse_range(component) {
+  try {
+    var parts = {};
+    var keys = ['start_date', 'end_date'];
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      if (component.extra[key]) {
+        var _parts = component.extra[key].split(' ');
+        parts[key] = {
+          value: parseInt(_parts[0].replace('+', '')),
+          interval: _parts[1]
+        };
+      } 
+    }
+    if (empty(parts)) { return null; }
+    return parts;
+  }
+  catch (error) { console.log('webform_date_component_parse_range - ' + error); }
+}
+
+/**
+ *
+ */
+function webform_results_container_id(nid) {
+  try {
+    return 'webform_results_container_' + nid;
+  }
+  catch (error) { console.log('webform_results_container_id - ' + error); }
+}
+
+/**
+ *
+ */
+function webform_submission_container_id(mode, nid, sid) {
+  try {
+    return 'webform_submission_container_' + mode + '_' + nid + '_' + sid;
+  }
+  catch (error) { console.log('webform_results_container_id - ' + error); }
+}
+
+/**
+ *
+ */
+function webform_tokens_replace(value) {
+  try {
+    if (!value) { return ''; }
+    var _value;
+    var _token;
+    var parts = value.split(' ');
+    for (var i = 0; i < parts.length; i++) {
+      _value = '';
+      _token = '';
+      if (parts[i].indexOf('%') == 0) {
+        if (parts[i].indexOf('%username') == 0 && Drupal.user.uid != 0) {
+          _token = '%username';
+          _value = Drupal.user.name;
+        }
+        else if (parts[i].indexOf('%useremail') == 0 && Drupal.user.uid != 0) {
+          _token = '%useremail';
+          _value = Drupal.user.mail;
+        }
+        else if (parts[i].indexOf('%ip_address') == 0) {
+          _token = '%ip_address';
+          _value = drupalgap_get_ip();
+        }
+        else if (parts[i].indexOf('%site') == 0) {
+          _token = '%site';
+          _value = drupalgap.settings.title;
+        }
+        else if (parts[i].indexOf('%nid') == 0) {
+          _token = '%nid';
+          _value = entity.nid;
+        }
+        else if (parts[i].indexOf('%title') == 0) {
+          _token = '%title';
+          _value = entity.title;
+        }
+      }
+      if (!empty(_value)) { value = value.replace(_token, _value); }
+    }
+    return value;
+  }
+  catch (error) { console.log('webform_tokens_replace - ' + error); }
+}
---
+++ src/webform.services.js
@@ -0,0 +1,132 @@
+/**
+ * SERVICES
+ */
+
+/**
+ * Creates a webform_submission.
+ * @param {Number} uuid
+ * @param {Object} submission
+ * @param {Object} options
+ */
+function webform_submission_create(uuid, submission, options) {
+  try {
+    options.method = 'POST';
+    options.data = JSON.stringify({
+      webform: uuid,
+      submission: submission
+    });
+    options.path = 'submission.json';
+    options.service = 'submission';
+    options.resource = 'create';
+    Drupal.services.call(options);
+  }
+  catch (error) { console.log('webform_submission_create - ' + error); }
+}
+
+/**
+ * Retrieves a webform_submission.
+ * @param {Number} nid
+ * @param {Number} sid
+ * @param {Object} options
+ */
+function webform_submission_retrieve(nid, sid, options) {
+  try {
+    options.method = 'GET';
+    options.path = 'webform_submission/' + nid + '/' + sid + '.json';
+    options.service = 'webform_submission';
+    options.resource = 'retrieve';
+    Drupal.services.call(options);
+  }
+  catch (error) { console.log('webform_submission_retrieve - ' + error); }
+}
+
+/**
+ * Update a webform_submission.
+ * @param {Number} nid
+ * @param {Number} sid
+ * @param {Object} webform_submission
+ * @param {Object} options
+ */
+function webform_submission_update(nid, sid, webform_submission, options) {
+  try {
+  }
+  catch (error) { console.log('webform_submission_update - ' + error); }
+}
+
+/**
+ * Delete a webform_submission.
+ * @param {Number} nid
+ * @param {Number} sid
+ * @param {Object} options
+ */
+function webform_submission_delete(nid, sid, options) {
+  try {
+  }
+  catch (error) { console.log('webform_submission_delete - ' + error); }
+}
+
+/**
+ *
+ */
+function webform_submissions(uuid, query, options) {
+  try {
+    options.method = 'GET';
+    options.path = 'webform/' + uuid + '/submissions.json' + webform_prepare_query_string(query);
+    options.service = 'webform';
+    options.resource = 'submissions';
+    Drupal.services.call(options);
+  }
+  catch (error) { console.log('webform_submissions - ' + error); }
+}
+
+/**
+ * Perform a webform_submission index.
+ * @deprecated
+ * @param {Object} query
+ * @param {Object} options
+ */
+function webform_submission_index(query, options) {
+  try {
+    alert('webform_submission_index is deprecated! Use webform_submissions() instead.');
+    return;
+    var query_string = webform_prepare_query_string(query);
+    Drupal.services.call({
+        method: 'GET',
+        path: 'submission.json' + query_string,
+        service: 'submission',
+        resource: 'index',
+        success: function(result) {
+          try {
+            if (options.success) { options.success(result); }
+          }
+          catch (error) { console.log('entity_index - success - ' + error); }
+        },
+        error: function(xhr, status, message) {
+          try {
+            if (options.error) { options.error(xhr, status, message); }
+          }
+          catch (error) { console.log('entity_index - error - ' + error); }
+        }
+    });
+  }
+  catch (error) { console.log('webform_submission_index - ' + error); }
+}
+
+/**
+ *
+ */
+function webform_prepare_query_string(query) {
+  try {
+    var query_string = '';
+    if (typeof query === 'object') {
+      query_string = entity_index_build_query_string(query);
+    }
+    else if (typeof query === 'string') {
+      query_string = query;
+    }
+    if (query_string) { query_string = '&' + query_string; }
+    else { query_string = ''; }
+    return query_string;
+  }
+  catch (error) { console.log('webform_prepare_query_string - ' + error); }
+}
